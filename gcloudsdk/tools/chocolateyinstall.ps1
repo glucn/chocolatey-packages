043ebfb9841f80fec7dd31c9f6bbd12a99b18f23
glucn <madcated.lu@gmail.com>
2024-08-05 00:19:59 -0700

    Upgrade gcloudsdk to 486.0.0

--- gcloudsdk/tools/chocolateyinstall.ps1
+++ gcloudsdk/tools/chocolateyinstall.ps1
@@ -1,17 +1,23 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://dl.google.com/dl/cloudsdk/channels/rapid/GoogleCloudSDKInstaller.exe'
-$checksum   = '707E2F76EF6A326B33F0A34944EE9E2E3F57A783BBCD63DAD22DDFC0662643E7'
+$url64        = 'https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-windows-x86_64.zip'
+$checksum64   = '9FD851D29CEB23145E002B6E2E5D349B423224C6A9FF8FDE0E65458E66CEF1E1'
+$url32        = 'https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-windows-x86.zip'
+$checksum32   = '5668956C3F9A5F713B2BE5812F1544454E54AF4550EAEC64314DDCA749B97717'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileType       = 'exe'
-  url            = $url
-  checksum       = $checksum
+  unzipLocation  = $toolsDir
+  url            = $url32
+  url64bit       = $url64
+  checksum       = $checksum32
   checksumType   = 'sha256'
-  silentArgs     = "/S /allusers"
-  validExitCodes = @(0, 3010, 1641)
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyZipPackage @packageArgs
+
+Write-Host "Adding to PATH if needed"
+Install-ChocolateyPath -Path "$($toolsDir)\google-cloud-sdk\bin" -PathType 'User'
