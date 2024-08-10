ed10c4c8c7243eb6a88a722e75028dabc353798a
glucn <madcated.lu@gmail.com>
2024-08-09 21:33:19 -0700

    Upgrade gcloudsdk to 487.0.0

--- gcloudsdk/tools/chocolateyinstall.ps1
+++ gcloudsdk/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64        = 'https://storage.googleapis.com/cloud-sdk-release/google-cloud-cli-486.0.0-windows-x86_64.zip'
-$checksum64   = '9FD851D29CEB23145E002B6E2E5D349B423224C6A9FF8FDE0E65458E66CEF1E1'
-$url32        = 'https://storage.googleapis.com/cloud-sdk-release/google-cloud-cli-486.0.0-windows-x86.zip'
-$checksum32   = '5668956C3F9A5F713B2BE5812F1544454E54AF4550EAEC64314DDCA749B97717'
+$url64        = 'https://storage.googleapis.com/cloud-sdk-release/google-cloud-cli-487.0.0-windows-x86_64.zip'
+$checksum64   = '2AF1FAB9D015E6A90AD0CA5D8241A417A2793098414C26873CE874FB457D0F16'
+$url32        = 'https://storage.googleapis.com/cloud-sdk-release/google-cloud-cli-487.0.0-windows-x86.zip'
+$checksum32   = '3E8C497C75A38DD5818182E3881CFC0657C88432C954EAE1153E7A3F78348F86'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
