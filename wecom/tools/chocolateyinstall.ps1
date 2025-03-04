6be938161a353f56d073bc1c0e9c6462e7af7117
glucn <madcated.lu@gmail.com>
2025-03-03 20:57:50 -0800

    Upgrade wecom to 4.1.33.6009

--- wecom/tools/chocolateyinstall.ps1
+++ wecom/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://work.weixin.qq.com/wework_admin/commdownload?platform=win'
-$checksum   = '3CF8EDB8309E165C6E6D09D5B2AD90902C33D1A0518AABD9BF6F358B740F80E9'
+$checksum   = '065F56C41514451820C8F4A1037133321106B7026F00991492B2F7AA235414A5'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
