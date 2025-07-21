21cc7962404ad5e360e9fa83841156068bd0a141
glucn <madcated.lu@gmail.com>
2025-07-20 23:28:58 -0700

    gcloudsdk.530.0.0

--- gcloudsdk/tools/chocolateyinstall.ps1
+++ gcloudsdk/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://storage.googleapis.com/cloud-sdk-release/google-cloud-cli-529.0.0-windows-x86_64.zip'
+$url64      = 'https://storage.googleapis.com/cloud-sdk-release/google-cloud-cli-530.0.0-windows-x86_64.zip'
 $checksum64 = '9653B04CF9042C8A2875E18B052F042D35287E246B988A5E59CF16860BFE16CC'
-$url32      = 'https://storage.googleapis.com/cloud-sdk-release/google-cloud-cli-529.0.0-windows-x86.zip'
+$url32      = 'https://storage.googleapis.com/cloud-sdk-release/google-cloud-cli-530.0.0-windows-x86.zip'
 $checksum32 = 'FC23DE35972B920A059006652591B2F76BE10941A88595A79D057424DAADDF9A'
 
 $packageArgs = @{
