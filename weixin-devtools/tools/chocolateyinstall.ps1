c96fe23dd033d58f2fb903e60fa2b6d533458be8
glucn <madcated.lu@gmail.com>
2025-08-20 23:04:30 -0700

    wechatdevtools.1.6.2504020

--- weixin-devtools/tools/chocolateyinstall.ps1
+++ weixin-devtools/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64        = 'https://servicewechat.com/wxa-dev-logic/download_redirect?type=win32_x64&from=mpwiki&download_version=1062503290&version_type=1'
-$checksum64   = 'DC9DB888B1C323F6C320ACDCC290B95414411A33B5B2C5481FA1B88E74CB0860'
-$url32        = 'https://servicewechat.com/wxa-dev-logic/download_redirect?type=win32_ia32&from=mpwiki&download_version=1062503290&version_type=1'
-$checksum32   = '256A86765C82C777BE3E1045C5E3209F4D46822B8DF893CA0F22323ADD0C7D05'
+$url64        = 'https://servicewechat.com/wxa-dev-logic/download_redirect?type=win32_x64&from=mpwiki&download_version=1062504020&version_type=1'
+$checksum64   = '34C0FC442656FD271B4CC211B1D8DA7BF6A4F6341E587153918238EDF6E7CA8E'
+$url32        = 'https://servicewechat.com/wxa-dev-logic/download_redirect?type=win32_ia32&from=mpwiki&download_version=1062504020&version_type=1'
+$checksum32   = 'E11F66D5711AD1F9FDCBE2960FB3D8957884FE1569D41A2F6E4B677C62F87AD2'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
