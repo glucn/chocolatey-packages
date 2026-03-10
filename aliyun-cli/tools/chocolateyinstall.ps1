617aa8e789fe0df0d1a48f5032460d81cc98511c
glucn <madcated.lu@gmail.com>
2026-03-09 19:15:15 -0700

    aliyun-cli.3.2.12

--- aliyun-cli/tools/chocolateyinstall.ps1
+++ aliyun-cli/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://aliyuncli.alicdn.com/aliyun-cli-windows-3.2.11-amd64.zip'
-$checksum64 = 'efb6c1d408061a3924a9f07d6892392394d028101ca647297bd5c8279fae6435'
+$url64 = 'https://aliyuncli.alicdn.com/aliyun-cli-windows-3.2.12-amd64.zip'
+$checksum64 = '5129d5fa56e27d5564d96d0a4db372ed3f9c52b38581b8899761f50083810964'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
